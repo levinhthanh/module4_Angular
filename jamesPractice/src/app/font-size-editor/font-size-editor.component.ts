c28cef6f10b40bbb23a0b430b58c1448e081861a
LE VINH THANH <levinhthanh.dt1@gmail.com>
2020-09-03 15:04:02 +0700

    add pet

---
+++ jamesPractice/src/app/font-size-editor/font-size-editor.component.ts
@@ -0,0 +1,25 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-font-size-editor',
+  templateUrl: './font-size-editor.component.html',
+  styleUrls: ['./font-size-editor.component.scss']
+})
+export class FontSizeEditorComponent implements OnInit {
+
+  fontSize = 14;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onChange(event) {
+    this.fontSize = event.target.value;
+    console.log(event);
+  }
+
+
+
+}
