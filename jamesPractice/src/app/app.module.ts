d2c2f3c832df53466913f415271d39dc538091db
LE VINH THANH <levinhthanh.dt1@gmail.com>
2020-09-04 11:12:44 +0700

    add list articles

--- jamesPractice/src/app/app.module.ts
+++ jamesPractice/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
 import { PetComponent } from './pet/pet.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { HackerNewsComponent } from './hacker-news/hacker-news.component';
+import { FormsModule } from '@angular/forms';
+import { LikesComponent } from './likes/likes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FontSizeEditorComponent,
     PetComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    HackerNewsComponent,
+    LikesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
